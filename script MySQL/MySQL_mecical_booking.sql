15b592576b96fc9d35d1caa63abfd3c0c5bada91
Nguyễn Trường Thành <thanhnthe150861@fpt.edu.vn>
2023-06-06 02:03:13 +0700

    Script MySQL

---
+++ script MySQL/MySQL_mecical_booking.sql
@@ -0,0 +1,160 @@
+drop database medical_booking;
+create database medical_booking;
+use medical_booking;
+
+
+CREATE TABLE bill(
+	id int auto_increment,
+	booking_id int NOT NULL,
+	payment_status varchar(20) NOT NULL,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE booking(
+	id int NOT NULL,
+	doctor_id int NOT NULL,
+	client_id int NOT NULL,
+	id_schedule int NOT NULL,
+	booking_reason varchar(255) NOT NULL,
+	status varchar(20) NOT NULL,
+	PRIMARY KEY  (id)
+);
+
+CREATE TABLE account(
+	username varchar(50) NOT NULL,
+	password varchar(50) NOT NULL,
+	phone_number varchar(20) NOT NULL,
+	email varchar(100) NOT NULL,
+    isAdmin int NOT NULL,
+	PRIMARY KEY (username)
+);
+
+CREATE TABLE client(
+	id int auto_increment,
+	username_account varchar(50) NOT NULL,
+	name varchar(150) NOT NULL,
+	rank_id int NULL,
+	PRIMARY KEY  (id)
+);
+
+
+CREATE TABLE doctor(
+	id int auto_increment,
+	username_account varchar(50) NOT NULL,
+	name varchar(150) NOT NULL,
+	specialty varchar(50) NOT NULL,
+	rank_id int NULL,
+	PRIMARY KEY  (id)
+);
+
+
+CREATE TABLE medical_record(
+	id int auto_increment,
+	booking_id int NOT NULL,
+	name varchar(150) NOT NULL,
+	dob date NOT NULL,
+	gender bit NOT NULL,
+	diagnosis varchar(100) NOT NULL,
+	medication varchar(100) NULL,
+	PRIMARY KEY  (id)
+);
+
+CREATE TABLE rank_client(
+	id int NOT NULL,
+	name varchar(150) NOT NULL,
+	PRIMARY KEY  (id)
+);
+
+CREATE TABLE rank_doctor(
+	id int NOT NULL,
+	name varchar(150) NOT NULL,
+    PRIMARY KEY  (id)
+);
+
+CREATE TABLE schedule(
+	id int auto_increment,
+	date date NULL,
+	PRIMARY KEY  (id)
+);
+
+CREATE TABLE schedule_client(
+	schedule_id int NOT NULL,
+	client_id int NOT NULL,
+	PRIMARY KEY  (client_id, schedule_id)
+);
+
+CREATE TABLE schedule_doctor(
+	schedule_id int NOT NULL,
+	doctor_id int NOT NULL,
+	PRIMARY KEY  (doctor_id,schedule_id)
+);
+
+CREATE TABLE slot(
+	id int NOT NULL,
+	name varchar(150) NOT NULL,
+	PRIMARY KEY  (id)
+);
+
+CREATE TABLE slot_booking(
+	schedule_id int NOT NULL,
+	slot_id int NOT NULL,
+	PRIMARY KEY  (schedule_id,slot_id)
+);
+
+
+
+INSERT account (username, password, phone_number, email, isAdmin) VALUES (N'admin', N'123456', N'1234567890', N'thanh17042001@gmail.com', N'0');
+INSERT rank_client (id, name) VALUES (1, N'Đồng'),(2, N'Bạc'),(3, N'Vàng'),(4, N'Kim Cương');
+select * from rank_client;
+select * from account;
+select * from client;
+delete from account where username = N'thanh';
+
+
+ALTER TABLE bill  ADD  CONSTRAINT FOREIGN KEY(booking_id)
+REFERENCES booking (id);
+
+ALTER TABLE booking  ADD  CONSTRAINT FOREIGN KEY(client_id)
+REFERENCES client (id);
+
+
+ALTER TABLE booking  ADD  CONSTRAINT FOREIGN KEY(doctor_id)
+REFERENCES doctor (id);
+
+
+ALTER TABLE booking  ADD  CONSTRAINT FOREIGN KEY(id_schedule)
+REFERENCES schedule (id);
+
+
+ALTER TABLE client  ADD  CONSTRAINT FOREIGN KEY(rank_id)
+REFERENCES rank_client (id);
+
+--
+ALTER TABLE client  ADD  CONSTRAINT FOREIGN KEY(username_account)
+REFERENCES account (username);
+
+
+ALTER TABLE doctor  ADD  CONSTRAINT FOREIGN KEY(rank_id)
+REFERENCES rank_doctor (id);
+
+-- 
+ALTER TABLE doctor  ADD  CONSTRAINT FOREIGN KEY(username_account)
+REFERENCES account (username);
+
+
+ALTER TABLE medical_record  ADD  CONSTRAINT FOREIGN KEY(booking_id)
+REFERENCES booking (id);
+
+
+ALTER TABLE slot_booking  ADD  CONSTRAINT FOREIGN KEY(schedule_id)
+REFERENCES schedule (id);
+
+
+ALTER TABLE slot_booking  ADD  CONSTRAINT FOREIGN KEY(slot_id)
+REFERENCES slot (id);
+
+
+
+
+
+
