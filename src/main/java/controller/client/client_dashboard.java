628b6144d68cb5cde0d150cccc2fe0e96df14064
Nguyễn Trường Thành <thanhnthe150861@fpt.edu.vn>
2023-06-16 00:01:40 +0700

    update dashboard for client

---
+++ src/main/java/controller/client/client_dashboard.java
@@ -0,0 +1,26 @@
+package controller.client;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "client_dashboard", value = "/client_dashboard")
+public class client_dashboard extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        HttpSession session = req.getSession();
+//        Account account = (Account) session.getAttribute("account");
+//        if (account != null && account.getIsAdmin() == 2){
+        req.getRequestDispatcher("view/client/client-dashboard.jsp").forward(req,resp);
+//        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
